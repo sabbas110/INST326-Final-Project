8582bdacd3656267f764cdf7bc743a50f839f23c
maxwellvoeltner <147751908+maxwellvoeltner@users.noreply.github.com>
2023-11-03 15:59:52 -0400

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -1,6 +1,33 @@
 import cws
 import misc
 
+#run it with:
+#python3 -m pytest StudentTests.py  
+'''
+heres the idea:
+
+def whatever-function-you're-testingTest()
+    expected = what the correct result should be
+    output = function-name(whatever you wanna test as a parameter)
+    assert(expect == output)
+
+
+examples (they dont work theyre just examples):
+
+def get_crosslistTest():
+    expected = [CCJS100, CCJS101]
+    output = get_crosslist("CCJS100")
+    assert(expect == output)
+
+*some functions return more than 1 thing like the parsers; they return a list and a flag(boolean).
+*youll have to do something like:
+    expected flag = False
+    expected output = [INST121, INST362]
+    output, flag = prereq1formatter("1 course with a minimum grade of C- from (INST121, INST326)")
+    assert(output == expected output)
+    assert(flag == expected flag)
+'''
+
 def creditsTest():
     expected = 3
     output = cws.get_credits("INST326")
