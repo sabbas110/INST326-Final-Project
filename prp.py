249f2fbce9d5ab03782c3ed375635467ac339c6e
maxwellvoeltner <147751908+maxwellvoeltner@users.noreply.github.com>
2023-11-03 15:17:13 -0400

    Update and rename parsers.py to prp.py

--- prp.py
+++ prp.py
@@ -1,13 +1,16 @@
 import re
 
-#doesnt work for this one yet
 # straightforward one or more courses format
 # #Prerequisite: MATH240, MATH241
 
 def prereq_format1_parser(prereq_description):
+    if "or" in prereq_description:
+        orFlag = True
+    else:
+        orFlag = False
     prereqs = prereq_description.replace("Prerequisite: ", "")
-    prereqs = prereqs.replace("and ", "").replace(",", "").split()
-    return prereqs
+    prereqs = prereqs.replace("and ", "").replace("or", "").replace(",", "").split()
+    return prereqs, orFlag
 
 
 # 1 course with minumum grade of C- from... format
@@ -20,6 +23,14 @@ def prereq_format2a_parser(prereq_description):
 
 def prereq_format2b_parser(prereq_description):
     prereq = re.search(r'(?<=from ).*', prereq_description).group()
+    andFlag = False
+    if "and" in prereq:
+        andFlag = True
+    prereq = prereq.replace("or ", "").replace("and ", "").replace(",", "").split(" ")
+    return prereq, andFlag
+
+def prereq_format2c_parser(prereq_description):
+    prereq = re.search(r'(?<=and \().*(?=\))', prereq_description).group()
     andFlag = False
     if "and" in prereq:
         andFlag = True
@@ -46,7 +57,6 @@ def prerequisite_assembler(prerequisites):
     assembled_prerequisites = []
 
     for prereq in prerequisites:
-        print(prereq)
 
         '''
         these conditionals search for substrings within the unassembled prerequisites
@@ -72,10 +82,18 @@ def prerequisite_assembler(prerequisites):
             else:
                 assembled_prerequisites.append(parsed_prereq)
 
+        elif "and (" in prereq:
+            parsed_prereq, andFlag = prereq_format2c_parser(prereq)
+            if andFlag == True or len(parsed_prereq) == 1:
+                for prereq in parsed_prereq:
+                    assembled_prerequisites.append(prereq)
+            # if the flag is false then that means any one of those classes suffice, so they should be entered together in a list
+            else:
+                assembled_prerequisites.append(parsed_prereq)
+
         elif "C- in" in prereq:
 
             parsed_prereq, andFlag = prereq_format3_parser(prereq)
-            print(andFlag)
 
             # if the andFlag is true, that means the courses should be entered seperately because they are all required
             # if there is only 1 course then it can just be entered seperately
@@ -86,12 +104,16 @@ def prerequisite_assembler(prerequisites):
             else:
                 assembled_prerequisites.append(parsed_prereq)
 
-        elif prereq.startswith("Prerequisite"):
-
-            parsed_prereq = prereq_format1_parser(prereq)
-
-            for prereq in parsed_prereq:
-                assembled_prerequisites.append(prereq)
+        elif prereq.startswith("Prerequisite: "):
+            if not any(char.isdigit() for char in prereq):
+                print(prereq)
+                continue
+            parsed_prereq, orFlag = prereq_format1_parser(prereq)
+            if orFlag == True:
+                assembled_prerequisites.append(parsed_prereq)
+            else:
+                for prereq in parsed_prereq:
+                    assembled_prerequisites.append(prereq)
 
     #return the list of assembled prerequisites after all iterations of prerequisites
     return assembled_prerequisites
@@ -105,3 +127,16 @@ def crosslist_parser(script):
     script = script.replace(",", "").replace("and ", "").replace("or ", "")
     crosslist = script.split()
     return crosslist
+
+def genEd_parser(genEd):
+    if "or" in genEd:
+        orFlag = True
+    genEd = genEd.replace("\t", "").replace("\n", " ").replace("GenEd:", "").replace(" ", "")
+    genEd = genEd.split(",")
+    if orFlag == True:
+        temp = []
+        genEd.split("or")
+        for gen in genEd:
+            temp.append(gen)
+        genEd = [temp]
+    return genEd
