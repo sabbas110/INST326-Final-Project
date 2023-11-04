ce12fad7c1c88cfdf3eded10ef5ad58e92495209
maxwellvoeltner <147751908+maxwellvoeltner@users.noreply.github.com>
2023-11-03 20:44:48 -0400

    Create requirementverification.py

---
+++ requirementverification.py
@@ -0,0 +1,18 @@
+def genEd_verification(graduation_plan):
+  '''determines whether a graduation plan fulfills the general education graduation requirements
+  Args:
+    graduation_plan(dict): dictionary with semester numbers as keys and lists of course objects as values
+  Returns:
+    satisfied(boolean): True if graduation plan satisfies all general education requirements; false otherwise
+  '''
+  pass
+
+def major_requirements_verification(major, graduation_plan):
+  '''determines whether a graduation plan fulfills the major requirements for the given major
+  Args:
+    major(str): name of the college major
+    graduation_plan(dict): dictionary with semester numbers as keys and lists of course objects as values
+  Returns:
+    satisfied(boolean): True if graduation plan satisfies all major requirements; false otherwise
+  '''
+  pass
