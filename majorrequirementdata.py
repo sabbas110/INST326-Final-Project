53a3f823fa2e3a5f60889f09352a7624acb3aacc
maxwellvoeltner <147751908+maxwellvoeltner@users.noreply.github.com>
2023-11-28 20:32:56 -0500

    Update majorrequirementdata.py

--- majorrequirementdata.py
+++ majorrequirementdata.py
@@ -2,10 +2,12 @@
 
 information_science_data = {
     
+    #required to complete
     "core_courses": ["PSYC100", "STAT100", "MATH115", "INST126", "INST201", 
                    "INST311", "INST314", "INST326", "INST327", "INST335", 
                    "INST346", "INST352", "INST362", "INST490"],
-                   
+    
+    #need 5 of these courses to complete elective requirement
     "electives": ["INST354", "INST377", "INST414", "INST447", "INST462", 
                   "INST341", "INST363", "INST364", "INST366", "INST367", "INST370", "INST371", "INST377", "INST380",
                   "INST388", "INST389", "INST398", "INST401", "INST402", "INST403", "INST405", "INST406", "INST407", "INST408",
@@ -14,34 +16,56 @@ information_science_data = {
                   "INST463", "INST464", "INST466", "INST467", "INST470"]
 }
 
-social_data_science = {
+social_data_science_data = {
     
+    #required to complete
     "core_courses": ["INST126", "INST326", "INST327", "INST366", 
                    "INST414", "INST447", "INST462", "SURV400", "SURV430", "INST492"],
     
-    "electives":
-        {"African American Studies track": ["STAT100", "MATH115", "AASP101", "BSOS233", "AASP210", "AASP395", "AASP301", "AASP310",
-         "AASP313", "AASP314", "AASP400", "AASP402", "AASP411", "AASP441", "AASP443"]},
+    "electives": [
+        "AASP101", "BSOS233", "AASP210", "AASP395", "AASP301", 
+        "AASP310", "AASP313", "AASP314", "AASP400", "AASP402", "AASP411", "AASP441", 
+        "AASP443",
         
-        "Anthropology track": ["ANTH210", "INST314", "ANTH310", "ANTH411", "ANTH412", "ANTH413", "ANTH415", "ANTH416", "ANTH341", "ANTH440", "ANTH441", "ANTH447", "ANTH448", "ANTH451", "ANTH464", "ANTH496", "ANTH450", "ANTH454", "ANTH456", "ANTH462", "ANTH467"],
+        "ANTH210", "INST314", "ANTH310", "ANTH411", "ANTH412", "ANTH413", "ANTH415", 
+        "ANTH416", "ANTH341", "ANTH440", "ANTH441", "ANTH447", "ANTH448", "ANTH451", 
+        "ANTH464", "ANTH496", "ANTH450", "ANTH454", "ANTH456", "ANTH462", "ANTH467",
         
-        "Economics track": ["MATH120", "ECON200", "ECON230", "ECON305", "ECON311", "ECON312", "ECON315", "ECON317", "ECON330", "ECON340"],
+        "MATH120", "ECON200", "ECON230", "ECON305", "ECON311", "ECON312", "ECON315", 
+        "ECON317", "ECON330", "ECON340",
         
-        "Geographical Sciences & GIS track": ["GEOG202", "GEOG306", "GEOG373", "GEOG330", "GEOG331", "GEOG332", "GEOG333", "GEOG335", "GEOG415", "GEOG416", "GEOG421", "GEOG422", "GEOG432", "GEOG470", "GEOG473", "GEOG475", "GEOG477"],
+        "GEOG202", "GEOG306", "GEOG373", "GEOG330", "GEOG331", "GEOG332", "GEOG333", 
+        "GEOG335", "GEOG415", "GEOG416", "GEOG421", "GEOG422", "GEOG432", "GEOG470", 
+        "GEOG473", "GEOG475", "GEOG477",
         
-        "Government & Politics & International Relations track": ["GVPT170", "GVPT200", "GVPT201", "GVPT320"],
+        "GVPT170", "GVPT200", "GVPT201", "GVPT320",
         
-        "Psychology": ["PSYC200", "PSYC300", "PSYC330", "PSYC332", "PSYC334", "PSYC336", "PSYC341", "PSYC344", "PSYC353", "PSYC354", "PSYC355", "PSYC356", "PSYC361", "PSYC362", "PSYC416", "PSYC417", "PSYC220", "PSYC424", "PSYC425", "PSYC432", "PSYC435", "PSYC436", "PSYC437", "PSYC440", "PSYC450", "PSYC456", "PSYC460"],
+        "PSYC200", "PSYC300", "PSYC330", "PSYC332", "PSYC334", "PSYC336", "PSYC341", 
+        "PSYC344", "PSYC353", "PSYC354", "PSYC355", "PSYC356", "PSYC361", "PSYC362", 
+        "PSYC416", "PSYC417", "PSYC220", "PSYC424", "PSYC425", "PSYC432", "PSYC435", 
+        "PSYC436", "PSYC437", "PSYC440", "PSYC450", "PSYC456", "PSYC460", 
         
-        "Sociology": ["SOCY100", "SOCY201", "SOCY202", "SOCY325", "SOCY335", "SOCY401", "SOCY405", "SOCY406", "SOCY407", "SOCY410", "SOCY411", "SOCY412", "SOCY413", "SOCY415", "SOCY420", "SOCY430", "SOCY431", "SOCY432", "SOCY441", "SOCY442", "SOCY444", "SOCY445", "SOCY457", "SOCY464", "SOCY465", "SOCY467", "SOCY480", "SOCY490", "SOCY491"],
+        "SOCY100", "SOCY201", "SOCY202", "SOCY325", "SOCY335", "SOCY401", "SOCY405", 
+        "SOCY406", "SOCY407", "SOCY410", "SOCY411", "SOCY412", "SOCY413", "SOCY415", 
+        "SOCY420", "SOCY430", "SOCY431", "SOCY432", "SOCY441", "SOCY442", "SOCY444", 
+        "SOCY445", "SOCY457", "SOCY464", "SOCY465", "SOCY467", "SOCY480", "SOCY490", 
+        "SOCY491",
         
-        "Public Health": ["SPHL100", "EPIB301", "EPIB315", "HLTH200", "EPIB330", "EPIB400", "EPIB463", "FMSC310", "FMSC332", "FMSC460", "HLSA300", "HLSA465", "HLSA484", "HLTH424", "HLTH434", "HLTH377", "KNES400", "KNES401", "MIEH240", "MIEH400", "PHSC401", "PHSC412"]
-        
-        }
+        "SPHL100", "EPIB301", "EPIB315", "HLTH200", "EPIB330", "EPIB400", "EPIB463", 
+        "FMSC310", "FMSC332", "FMSC460", "HLSA300", "HLSA465", "HLSA484", "HLTH424", 
+        "HLTH434", "HLTH377", "KNES400", "KNES401", "MIEH240", "MIEH400", "PHSC401", 
+        "PHSC412"
+    ]
+}
 
-technology_and_information_design = {
+technology_and_information_design_data = {
+
+    #required to complete
+    "core_courses": ["INST104", "INST126", "IDEA201", "INST201", "SOCY105", "STAT100",
+                    ["MATH110", "MATH112", "MATH113", "MATH115"], "INST204", "PLCY388D",
+                    "INST367", "INST406", "INST454", "INST466", "INST491"],
     
-    "core_courses": ["INST204", "PLCY388D", "INST367", "INST406", "INST454", "INST466", "NST491"],
-    
-    "electives": ["INST311", "INST352", "INST366", "INST401", "INST402", "INST404", "INST405", "INST441", "INST460", "INST463"]
+    #must complete 6 of these courses
+    "electives": ["INST311", "INST352", "INST366", "INST401", "INST402", "INST404",
+                  "INST404", "INST405", "INST441", "INST460", "INST463"]
 }
